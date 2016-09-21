b0fd76b69801523e63bdcdf8ee04a9c4f11395d3
btjd <btjd@users.noreply.github.com>
2016-09-21 00:21:32 -0400

    Coding exercises RotatedSearch(tweaks) and FloodFill

--- RotatedSearc.py
+++ RotatedSearc.py
@@ -10,35 +10,35 @@ O(1) in terms of space complexity. Time complexity is O(n + log n)
 
 def search_rotated(arr, item):
     found = False
+    start = 0
+    end = len(arr) - 1
     # If list is rotated and sorted then
     # last element is going to be smaller
     # than first
     if arr[0] > arr[-1]:
-        start = 0
-        end = len(arr)-1
         # We start by scanning from both ends. This loops stops when either our item or pivot
         # point is found
+        print "loop1"
         while start < end and arr[start] < arr[start+1] and arr[end] > arr[end-1] and not found:
             if arr[start] == item or arr[end] == item:
                 found = True
             else:
                 start += 1
                 end -= 1
-        # If we haven't found item and hit pivot point
-        # switch to binary search
-        while start <= end and not found:
-            mid = (start + end)//2
-            print mid
-            if arr[mid] == item:
-                found = True
+    # If we haven't found item and hit pivot point
+    # or if the array is rotated by n = len(arr)
+    # switch to binary search
+    while start <= end and not found:
+        print "loop2"
+        mid = (start + end)//2
+        if arr[mid] == item:
+            found = True
+        else:
+            if item < arr[mid]:
+                end = mid - 1
             else:
-                if item < arr[mid]:
-                    end = mid -1
-                else:
-                    start = mid + 1
-        return found
-    else:
-        print "The list you provided not rotated"
+                start = mid + 1
+    return found
 
-alist = [15, 16, 17, 18, 19, 20, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-print search_rotated(alist, 5)
+alist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+print search_rotated(alist, 20)
